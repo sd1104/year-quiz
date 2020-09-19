ea028c89683e892d3707b50e9077d29c5b678804
sd1104 <67669319+sd1104@users.noreply.github.com>
2020-09-19 22:57:38 +0900

    activeadmin installed

---
+++ app/admin/quizzes.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Quiz do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :title, :correct, :uncorrect1, :uncorrect2, :explanation, :lock_version
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:title, :correct, :uncorrect1, :uncorrect2, :explanation, :lock_version]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
