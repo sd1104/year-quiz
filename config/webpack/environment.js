0c69e672adc4dfcbed0f9f974bbc7938147bdc8f
sd1104 <67669319+sd1104@users.noreply.github.com>
2020-09-20 00:11:23 +0900

    environment settings

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -4,4 +4,12 @@ const vue = require('./loaders/vue')
 
 environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
 environment.loaders.prepend('vue', vue)
+const webpack = require('webpack')
+environment.plugins.append(
+    'Provide',
+    new webpack.ProvidePlugin({
+        jQuery: 'jquery/dist/jquery',
+        Popper: 'popper.js/dist/popper'
+    })
+)
 module.exports = environment
