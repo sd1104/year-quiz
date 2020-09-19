49e2d2ea1a34edb178745183b5061bc4ab954a25
sd1104 <67669319+sd1104@users.noreply.github.com>
2020-09-19 23:23:25 +0900

    api settings done

--- app/controllers/api/v1/quizzes_controller.rb
+++ app/controllers/api/v1/quizzes_controller.rb
@@ -1,23 +1,23 @@
 class Api::V1::QuizzesController < ApplicationController
   protect_from_forgery
-  before_action :set_employee, only: :show
+  before_action :set_quiz, only: :show
 
   rescue_from Exception, with: :render_status_500
 
   rescue_from ActiveRecord::RecordNotFound, with: :render_status_404
 
   def index
-    employees = Employee.all
-    render json: employees
+    quizzes = Quiz.all
+    render json: quizzes
   end
 
   def show
-    render json: @employee
+    render json: @quiz
   end
 
   private
-  def set_employee
-    @employee = Employee.find(params[:id])
+  def set_quiz
+    @quiz = Quiz.find(params[:id])
   end
 
   def render_status_404(exception)
