8d5e8050dd98ec786e72a083a4b11b95d60b7a83
sd1104 <67669319+sd1104@users.noreply.github.com>
2020-09-20 23:10:54 +0900

    header and footer component prepared

---
+++ app/javascript/QuizFooter.vue
@@ -0,0 +1,21 @@
+<template>
+  <p>{{ message }}</p>
+</template>
+
+<script>
+export default {
+  name: "QuizHeader",
+  data: function () {
+    return {
+      message: 'Footer Parts'
+    };
+  }
+}
+</script>
+
+<style scoped>
+p {
+  font-size: 2em;
+  text-align: center;
+}
+</style>
---
+++ app/javascript/QuizHeader.vue
@@ -0,0 +1,21 @@
+<template>
+  <p>{{ message }}</p>
+</template>
+
+<script>
+export default {
+  name: "QuizHeader",
+  data: function () {
+    return {
+      message: 'Header parts'
+    };
+  }
+}
+</script>
+
+<style scoped>
+p {
+  font-size: 2em;
+  text-align: center;
+}
+</style>
