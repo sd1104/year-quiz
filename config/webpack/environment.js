ee093d284137685d6e6efb95199cf4d47defd14c
sd1104 <67669319+sd1104@users.noreply.github.com>
2020-09-26 11:11:23 +0900

    webpacker settings

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -5,26 +5,6 @@ const vue = require('./loaders/vue')
 environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
 environment.loaders.prepend('vue', vue)
 const webpack = require('webpack')
-const path = require('path');
-const fs = require('fs');
-
-const nodeModules = {};
-fs.readdirSync('node_modules')
-  .filter(function(x) {
-    return ['.bin'].indexOf(x) === -1;
-  })
-  .forEach(function(mod) {
-    nodeModules[mod] = 'commonjs ' + mod;
-  });
-
-module.exports = {
-  entry: '/Users/satouhiroshi/projects/year-quiz/app/javascript/packs/hello_vue.js',
-  output: {
-    path: path.join(__dirname, 'build'),
-    filename: 'backend.js'
-  },
-  externals: nodeModules
-}
 environment.plugins.append(
     'Provide',
     new webpack.ProvidePlugin({
