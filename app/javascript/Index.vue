4657f6a552fb3eb5e6f71cdea53a65273cbe3488
sd1104 <67669319+sd1104@users.noreply.github.com>
2020-09-26 11:39:18 +0900

    debugged

--- app/javascript/Index.vue
+++ app/javascript/Index.vue
@@ -67,10 +67,10 @@ export default {
     return {
       currentSlide: 0,
       slides: [
-        { image: require('/Users/satouhiroshi/projects/year-quiz/app/assets/images/90s2.jpeg') },
-        { image: require('/Users/satouhiroshi/projects/year-quiz/app/assets/images/90s5.jpeg') },
-        { image: require('/Users/satouhiroshi/projects/year-quiz/app/assets/images/90s6.jpeg') },
-        { image: require('/Users/satouhiroshi/projects/year-quiz/app/assets/images/90s7.jpeg') }
+        { image: require('/Users/satouhiroshi/projects/year-quiz/app/assets/images/90s2.png') },
+        { image: require('/Users/satouhiroshi/projects/year-quiz/app/assets/images/90s5.png') },
+        { image: require('/Users/satouhiroshi/projects/year-quiz/app/assets/images/90s6.png') },
+        { image: require('/Users/satouhiroshi/projects/year-quiz/app/assets/images/90s7.png') }
       ]
     };
   },
