e85d0a68054d333c52868daa4d8e700ceb20a311
sd1104 <67669319+sd1104@users.noreply.github.com>
2020-10-01 08:28:19 +0900

    Update QuizHeader.vue

--- app/javascript/QuizHeader.vue
+++ app/javascript/QuizHeader.vue
@@ -7,17 +7,15 @@
         </span>
       </router-link>
 
-      <div @click="MenuToggle" v-if="menuToggle" v-html="menuBtn">
-        {{ menuBtn }}
+      <div class="hamburger_btn" @click=' menuToggle = !menuToggle '>
+        <span class="line line_01" v-bind:class="{'btn_line01':menuToggle}"></span>
+        <span class="line line_02" v-bind:class="{'btn_line02':menuToggle}"></span>
+        <span class="line line_03" v-bind:class="{'btn_line03':menuToggle}"></span>
       </div>
-      <div @click="MenuToggle" v-else v-html="closeBtn">
-        {{ closeBtn }}
-      </div>
-
     </nav>
 
-    <div>
-      <div class="menu-paper" v-if="menuPaper">
+    <transition name="menu">
+      <div class="menu-paper" v-show="menuToggle">
         <ul class="menu-paper-lists">
           <li class="menu-paper-list">
             <router-link :to="{ name: 'Index'}" id="router-link">
@@ -34,9 +32,8 @@
             </router-link>
           </li>
         </ul>
-
       </div>
-    </div>
+    </transition>
   </div>
 
 
@@ -47,7 +44,7 @@ export default {
   name: "QuizHeader",
   data: function () {
     return {
-      menuToggle: true,
+      menuToggle: false,
       menuBtn: '<i class="fas fa-bars"></i>',
       closeBtn: '<i class="fas fa-times"></i>',
       menuPaper: false,
